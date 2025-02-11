bf0030277d84d7877019729c52d8a2c9f432ee25
NikolajF <cph-nf70@cphbusiness.dk>
2025-02-11 15:37:30 +0100

    Work in progress. No idea why it doesn't work atm.

---
+++ Assets/Scripts/LightningSpawner.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class LightningSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject thunderStrikePrefab;
+    [SerializeField] private Camera playerCamera;
+
+    private int originalLayer;
+    private int ignoreRaycastLayer;
+    private GameObject player;
+
+    private void Start()
+    {
+        playerCamera = Camera.main;
+        player = GameObject.FindWithTag("Player");
+        originalLayer = gameObject.layer;
+        ignoreRaycastLayer = LayerMask.NameToLayer("Ignore Raycast");
+    }
+
+    public void CastLightning(Vector3 position)
+    {
+        player.layer = ignoreRaycastLayer;
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        
+        if (Physics.Raycast(ray, out hit))
+        {
+            // If the ray hits something, get the hit position and spawn the lightning on that point
+            Vector3 lightningPosition = hit.point;
+            Debug.Log(hit.point);
+
+            // Instantiate the lightning
+            Instantiate(thunderStrikePrefab, lightningPosition, Quaternion.identity);
+        }
+
+        player.layer = originalLayer;
+    }
+}
---
+++ Assets/Scripts/ThunderStrikeEffect.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class ThunderStrikeEffect : MonoBehaviour
+{
+    public float fallSpeed = 5f;
+    private bool isStriking = false;
+
+    private void Update()
+    {
+        if (isStriking)
+        {
+            transform.position += Vector3.down * (fallSpeed * Time.deltaTime);
+        }
+    }
+
+    public void StartStrike()
+    {
+        isStriking = true;
+    }
+}
